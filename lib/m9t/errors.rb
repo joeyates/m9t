fb58ca3475276336a17e659bc2061377688c24d7
Joe Yates <joe.g.yates@gmail.com>
2021-05-30 21:49:31 +0200

    Wrap code in module declaration

--- lib/m9t/errors.rb
+++ lib/m9t/errors.rb
@@ -1,7 +1,9 @@
-# Base class for all M9t exceptions
-class M9t::M9tError < StandardError
-end
+module M9t
+  # Base class for all M9t exceptions
+  class M9tError < StandardError
+  end
 
-# Raised when a M9t class receives an unrecogized ':units' value
-class M9t::UnitError < M9t::M9tError
+  # Raised when a M9t class receives an unrecogized ':units' value
+  class UnitError < M9t::M9tError
+  end
 end
